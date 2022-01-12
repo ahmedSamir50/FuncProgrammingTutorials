1c78b958a8ab510bc35d25b7cec889e02f55d847
ahmedSamir50 <ahmed.samir.abdelaal@gmail.com>
2022-01-12 23:55:10 +0200

    session 1 , 2

---
+++ MainConcepts/SequentialImparative.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FuncProgrammingTutorials.MainConcepts
+{
+    public class SequentialImparative : SequentialMethods
+    {
+        public static List<double> ImparativeMethod(List<double> listOfData)
+        {
+            Console.WriteLine("*****ImparativeMethod*****");
+            foreach (double item in listOfData)
+            {
+                Console.WriteLine(SubstractTen(Square(AddOne(item))).ToString());
+            }
+            return listOfData;
+        }
+    }
+    public class SequentialDeclarative : SequentialMethods
+    {
+        public static List<double> DeclarativeMethod(List<double> listOfData)
+        {
+            Console.WriteLine("*****Declarative Method*****");
+            listOfData.Select(AddOne)
+                      .Select(Square)
+                      // adding conditions is much easer in declarative 
+                      .Where(x=>x < 20)
+                      .Select(SubstractTen)
+                      .ToList();
+            foreach (double item in listOfData)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            return listOfData;
+        }
+    }
+
+}
---
+++ MainConcepts/SequentialImparativeBase.cs
@@ -0,0 +1,11 @@
+﻿namespace FuncProgrammingTutorials.MainConcepts
+{
+    public class SequentialMethods
+    {
+
+        public static double AddOne(double toAddOneTo) => ++toAddOneTo;
+
+        public static double Square(double toSquare) => toSquare * toSquare;
+        public static double SubstractTen(double toSub) => toSub - 10;
+    }
+}
